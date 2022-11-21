2bf118bc5b486c2a358bdd0bff4419e6ffb3c65e
Ayush Singh <ayushsinghkushwah8@gmail.com>
2022-11-21 23:16:00 +0530

    updating repo

---
+++ project/src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../common/Navbar';
+import './Home.css';
+import Header from '../common/Header';
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
---
+++ project/src/Pages/common/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <h2>this is Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
